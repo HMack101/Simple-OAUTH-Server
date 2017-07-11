ed9bb541acd45b23865bc537a15e844b61046e2e
Harvey McCreary III <dasmartest@gmail.com>
2017-07-10 22:33:34 -0500

    no message

---
+++ java/SimpleOAuthServer/src/main/java/com/hmack101/simpleoauthserver/SimpleOAuthServer.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.hmack101.simpleoauthserver;
+
+import com.hmack101.simpleoauthserver.api.ResourceHandler;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.servlet.ServletHolder;
+import org.glassfish.jersey.server.ResourceConfig;
+import org.glassfish.jersey.servlet.ServletContainer;
+
+/**
+ *
+ * @author HMack101
+ */
+public class SimpleOAuthServer {
+        
+    private static Logger log = Logger.getLogger(SimpleOAuthServer.class.getName());
+    
+    @SuppressWarnings("empty-statement")
+    public static void main(String[] args) {
+        // TODO code application logic here
+        ResourceConfig config = new ResourceConfig(ResourceHandler.class);
+        config.register(AuthenticationFilter.class);
+        
+        ServletHolder servlet = new ServletHolder(new ServletContainer(config));
+
+        Server server = new Server(8080);
+        ServletContextHandler context = new ServletContextHandler(server, "/");
+        context.addServlet(servlet, "/*");
+
+
+        try {
+            server.start();
+            server.join();
+        } catch (Exception ex) {
+            log.log(Level.SEVERE, null, ex);
+        } 
+        
+        server.destroy();
+       
+    }
+    
+}
+
+    
+}
---
+++ java/SimpleOAuthServer/src/main/java/com/hmack101/simpleoauthserver/api/ResourceHandler.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.hmack101.simpleoauthserver.api;
+
+/**
+ *
+ * @author HMack101
+ */
+public class ResourceHandler {
+    
+}
